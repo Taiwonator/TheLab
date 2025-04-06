52ff4dbb71fbd28541c81dfd189e67aa3ffe7050
Michael Taiwo <22432591+Taiwonator@users.noreply.github.com>
2025-04-06 19:34:29 +0100

    testing

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -33,7 +33,7 @@ export default buildConfig({
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
   db: mongooseAdapter({
-    url: process.env.NF_MONGO_PAYLOAD_MONGO_SRV || process.env.MONGODB_URI || '',
+    url: process.env.MONGODB_URI || process.env.EXTERNAL_SRV || '',
   }),
   sharp,
   plugins: [
