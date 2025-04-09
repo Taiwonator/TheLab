d6b49227d11b9613de4631801dbec091f9b9f63c
Michael Taiwo <22432591+Taiwonator@users.noreply.github.com>
2025-04-09 11:56:18 +0100

    feat(shadcn): adds shadcn

--- src/app/(frontend)/quest/page.tsx
+++ src/app/(frontend)/quest/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from '@(frontend)/components/ui/button'
+
 const QuestPage = () => {
   return (
     <div className="quest-page">
       <h1>Quest Page</h1>
       <p>This is the quest page.</p>
+      <Button>Button</Button>
     </div>
   )
 }
